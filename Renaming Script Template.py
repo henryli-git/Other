e056641c20dee4d4a66f2ca0f9bf38d4d4fa38cc
Henry Li <henry.li.github@gmail.com>
2022-03-26 09:59:44 -0700

    Add files via upload

---
+++ Renaming Script Template.py
@@ -0,0 +1,13 @@
+
+
+# Useful functions in creating a file renaming bot
+    # import os
+    # os.chdir()
+    # os.listdir()
+    # os.path.splitext()
+    # os.path.split()
+    # os.rename()
+    # .strip()
+    # .zfill()
+
+
